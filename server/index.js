2023f474a840a45ef4fcc11f4d89f7122929805c
Klimixxx <156718095+Klimixxx@users.noreply.github.com>
2025-08-20 01:02:25 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
